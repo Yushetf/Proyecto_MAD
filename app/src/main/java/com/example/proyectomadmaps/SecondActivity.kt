1b0679ba997a567af7da92526bb6b673a0b8bfc8
Yushetf <yushetf.lopez.jimenez@alumnos.upm.es>
2024-02-20 16:21:43 +0100

    Include some dialogues to provide feedback on the actions performed on the app. Use shared preferences to persist data that might be relevant across activities. Use files to persist information

--- app/src/main/java/com/example/proyectomadmaps/SecondActivity.kt
+++ app/src/main/java/com/example/proyectomadmaps/SecondActivity.kt
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
+import java.io.IOException
 
 class SecondActivity : AppCompatActivity() {
     private val TAG = "btaSecondActivity"
@@ -28,5 +30,24 @@ class SecondActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        // Display the file contents
+        val tvFileContents: TextView = findViewById(R.id.tvFileContents)
+        tvFileContents.text = readFileContents()
+
     }
+
+    private fun readFileContents(): String {
+        val fileName = "gps_coordinates.csv"
+        return try {
+            // Open the file from internal storage
+            openFileInput(fileName).bufferedReader().useLines { lines ->
+                lines.fold("") { some, text ->
+                    "$some\n$text"
+                }
+            }
+        } catch (e: IOException) {
+            "Error reading file: ${e.message}"
+        }
+    }
+
 }
