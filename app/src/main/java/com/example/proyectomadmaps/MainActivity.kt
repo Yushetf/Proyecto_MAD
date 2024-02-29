c5e50efad635aa5095b962f6531f02f1b3526de1
Yushetf <yushetf.lopez.jimenez@alumnos.upm.es>
2024-03-01 00:09:34 +0100

    A navigation menu has been created to replace the previous buttons. A SettingsActivity has been created to display the stored configuration. And a new purpose has been given to the SecondActivity: now it stores locations.

--- app/src/main/java/com/example/proyectomadmaps/MainActivity.kt
+++ app/src/main/java/com/example/proyectomadmaps/MainActivity.kt
@@ -2,19 +2,22 @@ package com.example.proyectomadmaps
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.util.Log
-import android.widget.Button
+import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
-import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.app.ActivityCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import java.io.File
 
 class MainActivity : AppCompatActivity(), LocationListener {
@@ -29,25 +32,9 @@ class MainActivity : AppCompatActivity(), LocationListener {
 
         Log.d(TAG, "onCreate: Estamos en actividad principal")
 
-        val buttonGoToSecond: Button = findViewById(R.id.button)
-        buttonGoToSecond.setOnClickListener {
-            val intent = Intent(this, SecondActivity::class.java)
-            startActivity(intent)
-        }
-
-        val buttonOsm: Button = findViewById(R.id.MapButton)
-        buttonOsm.setOnClickListener {
-            if (latestLocation != null) {
-                val intent = Intent(this, OpenStreetMapActivity::class.java)
-                val bundle = Bundle()
-                bundle.putParcelable("location", latestLocation)
-                intent.putExtra("locationBundle", bundle)
-                startActivity(intent)
-            } else {
-                Log.e(TAG, "Location not set yet.")
-                // Aquí podrías mostrar un mensaje al usuario informando que la ubicación no está disponible aún.
-            }
-        }
+        // Configure Toolbar
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
         locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
 
@@ -69,24 +56,68 @@ class MainActivity : AppCompatActivity(), LocationListener {
                 locationPermissionCode
             )
         } else {
-            // The location is updated every 5000 milliseconds (or 5 seconds) and/or if the device moves more than 5 meters,
-            // whichever happens first
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5f, this)
         }
 
         // Check if the user identifier is already saved
         val userIdentifier = getUserIdentifier()
         if (userIdentifier == null) {
-            // If not, ask for it
             askForUserIdentifier()
         } else {
-            // If yes, use it or show it
             Toast.makeText(this, "User ID: $userIdentifier", Toast.LENGTH_LONG).show()
         }
 
+        // ButtomNavigationMenu
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        navView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.navigation_map -> {
+                    if (latestLocation != null) {
+                        val intent = Intent(this, OpenStreetMapActivity::class.java)
+                        val bundle = Bundle()
+                        bundle.putParcelable("location", latestLocation)
+                        intent.putExtra("locationBundle", bundle)
+                        startActivity(intent)
+                    }else{
+                        Log.e(TAG, "Location not set yet.")
+                    }
+                    true
+                }
+                R.id.navigation_list -> {
+                    val intent = Intent(this, SecondActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> {
+                startActivity(Intent(this, SettingsActivity::class.java))
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == locationPermissionCode) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
@@ -98,7 +129,12 @@ class MainActivity : AppCompatActivity(), LocationListener {
                         android.Manifest.permission.ACCESS_COARSE_LOCATION
                     ) == PackageManager.PERMISSION_GRANTED
                 ) {
-                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5f, this)
+                    locationManager.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER,
+                        5000,
+                        5f,
+                        this
+                    )
                 }
             }
         }
@@ -145,15 +181,20 @@ class MainActivity : AppCompatActivity(), LocationListener {
     override fun onLocationChanged(location: Location) {
         latestLocation = location
         val textView: TextView = findViewById(R.id.mainTextView)
-        Toast.makeText(this, "Coordinates update! [${location.latitude}][${location.longitude}]", Toast.LENGTH_LONG).show()
-        textView.text = "Latitude: [${location.latitude}], Longitude: [${location.longitude}], UserId: [${getUserIdentifier()}]"
+        Toast.makeText(
+            this,
+            "Coordinates update! [${location.latitude}][${location.longitude}]",
+            Toast.LENGTH_LONG
+        ).show()
+        textView.text =
+            "Latitude: [${location.latitude}], Longitude: [${location.longitude}], UserId: [${getUserIdentifier()}]"
         saveCoordinatesToFile(location.latitude, location.longitude)
     }
+
     private fun saveCoordinatesToFile(latitude: Double, longitude: Double) {
         val fileName = "gps_coordinates.csv"
         val file = File(filesDir, fileName)
         val timestamp = System.currentTimeMillis()
         file.appendText("$timestamp;$latitude;$longitude\n")
     }
-
 }
--- app/src/main/java/com/example/proyectomadmaps/ThirdActivity.kt
+++ app/src/main/java/com/example/proyectomadmaps/ThirdActivity.kt
@@ -8,15 +8,14 @@ import android.widget.Button
 
 class ThirdActivity : AppCompatActivity() {
     private val TAG = "btaThirdActivity"
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third)
-
-        Log.d(TAG, "onCreate: Estamos en la tercera actividad");
-
-        val buttonGoToSecond: Button = findViewById(R.id.button4)
-        buttonGoToSecond.setOnClickListener {
+        val latitude = intent.getStringExtra("latitude")
+        val longitude = intent.getStringExtra("longitude")
+        Log.d(TAG, "Latitude: $latitude, Longitude: $longitude")
+        val buttonPrevious: Button = findViewById(R.id.button4)
+        buttonPrevious.setOnClickListener {
             val intent = Intent(this, SecondActivity::class.java)
             startActivity(intent)
         }
